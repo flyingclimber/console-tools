935834df06e97f36802428a003611571d47b96b1
Tomasz Finc <tomasz@gmail.com>
2013-06-27 19:48:09 -0700

    Appeasing the pylint gods

---
+++ MegaCD/readsystemid.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""readSystemID - Simple MegaCD system id reader"""
+
+SECTORSIZE = 2048
+SECTORSTOREAD = 1
+
+SECTORDATA = file("test.iso",'rb').read(SECTORSTOREAD*SECTORSIZE)
+DATAFILE = open("header",'w')
+
+DATAFILE.write(SECTORDATA)
+DATAFILE.close()
