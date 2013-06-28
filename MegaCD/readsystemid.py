6251d10d04cef75defeb97b117d8ee9e9a32fc60
Tomasz Finc <tomasz@gmail.com>
2013-06-28 12:17:03 -0700

    Adding game type, reference, version, and version number

--- MegaCD/readsystemid.py
+++ MegaCD/readsystemid.py
@@ -16,6 +16,10 @@ SYSTEMNAME = DATAFILE.read(0x10)
 COPYRIGHT = DATAFILE.read(0x10)
 DOMESTICNAME = DATAFILE.read(0x30)
 OVERSEASNAME = DATAFILE.read(0x30)
+GAMETYPE = DATAFILE.read(0x3)
+GAMEREFERENCE = DATAFILE.read(0x7)
+GAMEVERSION = DATAFILE.read(0x2)
+GAMEVERSIONNUMBER = DATAFILE.read(0x2)
 
 DATAFILE.seek(0x1F0)
 
@@ -28,6 +32,10 @@ print 'System name: ' + SYSTEMNAME
 print 'Copyright: ' + COPYRIGHT
 print 'Domestic name: ' + DOMESTICNAME
 print 'Overseas name: ' + OVERSEASNAME
+print 'Game type: ' + GAMETYPE
+print 'Game reference: ' + GAMEREFERENCE
+print 'Game version: ' + GAMEVERSION
+print 'Game version number: ' + GAMEVERSIONNUMBER
 print 'Country: ' + COUNTRY
 
 DATAFILE.close()
