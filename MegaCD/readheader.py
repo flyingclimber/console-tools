e319f27a292a7a5d009f423a2733e24bc17100d4
Tomasz Finc <tomasz@gmail.com>
2013-07-02 18:21:18 -0700

    for consistency

--- MegaCD/readheader.py
+++ MegaCD/readheader.py
@@ -18,7 +18,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-    readsystemid - simple MegaCD header reader
+    readheader - simple MegaCD header reader
 '''
 
 import io
